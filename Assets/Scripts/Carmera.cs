42385a63eae0cd98bd047f8e70aff42145e616c3
adballon <jeongm0841@daum.net>
2023-08-07 15:06:27 +0900

    몬스터 스폰

--- Assets/Scripts/Carmera.cs
+++ Assets/Scripts/Carmera.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Carmera : MonoBehaviour
 {
+    GameObject RoomPos;
+    GameObject Hero;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, -10);
+        RoomPos = GameObject.Find("");
+        Hero = GameObject.Find("hero");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position = Hero.GetComponent<Hero1>().transform.position;
     }
 }
--- Assets/Scripts/Map/RandomMapManager.cs
+++ Assets/Scripts/Map/RandomMapManager.cs
@@ -6,7 +6,6 @@ public class RandomMapManager : MonoBehaviour
 {
     public Roomcode[] roomsamples;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +21,7 @@ public class RandomMapManager : MonoBehaviour
     void CreateMap()
     {
         // 1. 시작점 방과 끝 방을 선택해서 놓는다.
-        // 2. 이전 방의 문의 개수만큼 
-        // 3. 
+        // 2. 방안의 몬스터의 개수가 0이 된다면 들어온 문을 제외한 문을 개방
+        // 3. 문으로 이동 한다면 다른 방으로 이동
     }
 }
